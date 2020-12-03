dd623cae5fb80b6fb418b51239412ca79eed640e
Caspento <Mrcaspento@gmail.com>
2020-12-03 18:39:32 -0500

    server.js

--- server.js
+++ server.js
@@ -5,36 +5,54 @@ const fs = require("fs");
 const app = express();
 
 const PORT = process.env.PORT || 3001;
-let notesData = [];
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "Develop/public")));
 
 
 
+const writefileAsync = util.promisify(fs.writeFile);
+const readFileAsync = util.promisify(fs.readFile);
+let sockNotes;
 
 
-
-
-
-
-
-
+app.post("/api/notes", function (req, res) {
+    var newNote = req.body;
+    readFileAsync(path.join(__dirname, "./Develop/db/db.json"), "utf8")
+        .then(function (data) {
+            sockNotes = JSON.parse(data);
+            if (newNote.id || newNote.id === 0) {
+                let currNote = sockNotes[newNote.id];
+                currNote.title = newNote.title;
+                currNote.text = newNote.text;
+            } else {
+                sockNotes.push(newNote);
+            }
+            writefileAsync(path.join(__dirname, "./Develop/db/db.json"), JSON.stringify(sockNotes))
+                .then(function () {
+                    console.log("socks");
+                })
+        });
+    res.json(newNote);
+});
 
 app.delete("/api/notes/:id", function (req, res) {
-    console.log(req.params.id);
-    var deleteID = req.params.id;
-    notes.splice(deleteID, 1);
-    assignID();
-    fs.writeFileSync("/Develop/db/db.json", JSON.stringify(notes), function (err) {
-        if (err) 
-            throw err
+    var id = req.params.id;
+    readFileAsync(path.join(__dirname, "./db/db.json"), "utf8")
+        .then(function (data) {
+            sockNotes = JSON.parse(data);
+            sockNotes.splice(id, 1);
+            writefileAsync(path.join(__dirname, "./db/db.json"), JSON.stringify(sockNotes))
+                .then(function () {
+                    console.log("Deleted db.json");
+                })
         });
-    res.json({deletion:"success"})
+    res.json(id);
 });
 
 
-function assignUniqueID() {
+function assignID() {
     for (i = 0; i < notes.length; i ++) {
         notes[i].id = i;
     }
@@ -50,10 +68,13 @@ app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "/public/assets/index.html"));
   });
   
-  app.get("/api/notes", function(req, res) {
-    return res.sendFile(path.json(__dirname, "Develop/db/db.json"));
-  });
-  
+  app.get("/api/notes", function (req, res) {
+    readFileAsync(path.join(__dirname, "/Develop/db/db.json"), "utf8")
+        .then(function (data) {
+            return res.json(JSON.parse(data));
+        });
+});
+
   app.listen(PORT, function() {
     console.log("server listening " + PORT);
   });
