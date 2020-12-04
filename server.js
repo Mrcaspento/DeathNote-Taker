374f49e1096be218a4d0ddabd38bd321b29c8dfe
Caspento <Mrcaspento@gmail.com>
2020-12-04 14:19:38 -0500

    typos

--- server.js
+++ server.js
@@ -5,65 +5,55 @@ const fs = require("fs");
 const app = express();
 
 var PORT = process.env.PORT || 3000; 
+//shortcut keys for routing
+const stickySock = path.join(__dirname, "./public/assets");
+const wetSock = path.join(__dirname, "./Develop/db");
 
-const stickySock = path.join(__dirname, "/public/assets");
-const wetSock = path.join(__dirname, "/Develop/db");
-app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
-app.get("/notes", function(req, res) {
-    res.sendFile(path.join(stickySock, "notes.html"));
-});
 
+//basic route that sends teh user first to the AJAX page
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(stickySock, "notes.html"))
+});
 app.get("*", function(req, res) {
     res.sendFile(path.join(stickySock, "index.html"));
 });
 
-
+//display notes  and store
 app.get("/api/notes", function(req, res) {
-    res.sendFile(path.join(wetSock, "db.json"));
+    return res.json(activeNote)
 });
-
-
 
 app.get("/api/notes/:id", function(req, res) {
-    let savedNotes = JSON.parse(fs.readFileSync(wetSock,"db.json", "utf8"));
-    res.json(savedNotes[Number(req.params.id)]);
+    let  = JSON.parse(fs.readFileSync(wetSock,"db.json", "utf8"));
+    res.json([Number(req.params.id)]);
 });
+app.post("/api/notes", function(req, res){
+    var newTable = req.body;
+  console.log(newTable)
+    if(tableList.length < 5){
 
-
-
-app.post("/api/notes", function(req, res) {
-    let savedNotes = JSON.parse(fs.readFileSync(wetSock,"db.json", "utf8"));
-    let newNote = req.body;
-    let uniqueID = (savedNotes.length).toString();
-    newNote.id = uniqueID;
-    savedNotes.push(newNote);
-
-    fs.writeFileSync(wetSock,"db.json", JSON.stringify(savedNotes));
-    console.log("Note saved to db.json. Content: ", newNote);
-    res.json(savedNotes);
-})
-
-app.delete("/api/notes/:id", function(req, res) {
-    let savedNotes = JSON.parse(fs.readFileSync(wetSock,"db.json", "utf8"));
-    let noteID = req.params.id;
-    let newID = 0;
-    console.log(`Deleting note with ID ${noteID}`);
-    savedNotes = savedNotes.filter(currNote => {
-        return currNote.id != noteID;
-    })
-    
-    for (currNote of savedNotes) {
-        currNote.id = newID.toString();
-        newID++;
+      tableList.push(newTable)
+    }else{
+      waitList.push(newTable)
     }
-
-    fs.writeFileSync(wetSock,"db.json", JSON.stringify(savedNotes));
-    res.json(savedNotes);
+    res.json(newTable);
 })
+//uniquie id script
+var noteid = Math.random() // 0.9394456857981651
+noteid.toString(36); 
+var id = noteid.toString(36).substr(2, 9); 
+id.length >= 9;
 
+
+
+// app.post("app/clear", function(req,res){
+//     tableData.splice(0);
+//     waitlistData.lenght = 0;
+//     res.json({ok: true});
+// })
   app.listen(PORT, function() {
     console.log("server listening " + PORT);
   });
