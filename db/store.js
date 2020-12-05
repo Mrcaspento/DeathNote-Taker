dc99afaf35a869452e21c0e83c1df5b5b2a2e23c
Caspento <Mrcaspento@gmail.com>
2020-12-04 19:52:49 -0500

    got the data to store to the JSON

---
+++ db/store.js
@@ -0,0 +1,27 @@
+
+// google mvc...... this is basically a 'model'
+const util = require('util');
+const fs = require('fs')
+
+// this makes read and write async so we can do .then()
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
+
+const uuidv1 = require('uuid')
+// go to postman and test GET /api/notes try it in chrome
+class Store{
+    retrive() {
+        return readFileAsync("db/db.json", "utf8")
+    }
+    getNotes() {
+        // 'this' refers to this class, Store
+        return this.retrive().then(function (notes) {
+            let formatedNotes = [].concat(JSON.parse(notes))
+            return formatedNotes
+        })
+    }
+
+    
+}
+// bruh
+module.exports = new Store();
--- server.js
+++ server.js
@@ -1,33 +1,45 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
+const Store = require('./db/store')
 
 const app = express();
 
 var PORT = process.env.PORT || 3000; 
 //shortcut keys for routing
-const stickySock = path.join(__dirname, "./public/assets");
+const stickySock = path.join(__dirname, "./public");
 const wetSock = path.join(__dirname, "./Develop/db");
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
-
+const saveNotes = [];
 //basic route that sends teh user first to the AJAX page
 app.get("/notes", function(req, res) {
-    res.sendFile(path.join(stickySock, "notes.html"))
+    res.sendFile(path.join(stickySock, "/notes.html"))
 });
-app.get("*", function(req, res) {
-    res.sendFile(path.join(stickySock, "index.html"));
+app.get("/", function(req, res) {
+    res.sendFile(path.join(stickySock, "/index.html"));
 });
 
+
+
+
+
+// we goin to use the classy boi Store to do the db manipulation
 //display notes  and store
 app.get("/api/notes", function(req, res) {
-    return res.json(activeNote)
+    Store.getNotes().then(function(notes) {
+      // send that sweet data in json format back to the front end guys who made this ajax call
+      res.json(notes)
+    }).catch(function(err){
+      // watching for errors bc we paranoid.... THEY'RE IN THE TREES!!lo!l
+      res.status(500).json(err)
+    })
 });
 
 app.get("/api/notes/:id", function(req, res) {
-    let  = JSON.parse(fs.readFileSync(wetSock,"db.json", "utf8"));
+    let  = JSON.parse(fs.readFileSync(wetSock,"/db.json", "utf8"));
     res.json([Number(req.params.id)]);
 });
 app.post("/api/notes", function(req, res){
@@ -44,11 +56,11 @@ app.post("/api/notes", function(req, res){
 
 
 
-// app.post("app/clear", function(req,res){
-//     tableData.splice(0);
-//     waitlistData.lenght = 0;
-//     res.json({ok: true});
-// })
+app.post("app/clear", function(req,res){
+    tableData.splice(0);
+    waitlistData.length = 0;
+    res.json({ok: true});
+})
   app.listen(PORT, function() {
     console.log("server listening " + PORT);
   });
