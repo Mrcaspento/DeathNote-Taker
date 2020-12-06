12a363b9be2d1099f879abc6c44eca093ab4c280
Caspento <Mrcaspento@gmail.com>
2020-12-05 23:54:50 -0500

    heroku

--- server.js
+++ server.js
@@ -1,61 +1,52 @@
-const express = require("express");
-const path = require("path");
-const fs = require("fs");
-const { dirname } = require('path');
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const uuid = require('uuid');
+
 const app = express();
-const stickySock = path.join(__dirname, "/public")
-var PORT = process.env.PORT || 3000;
-//shortcut keys for routing
+const PORT = process.env.PORT || 3001;
 
-
-app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
 
-//routes
-
-app.get("/notes", function (req, res) {
-  res.sendFile(path.join(stickySock, "notes.html"));
+let notes = [];
+const stickySock = path.join(__dirname, "/public")
+app.get("/", function(req, res) {
+    res.sendFile(path.join(stickySock, "index.html"));
 });
 
-app.get("/api/notes", function (req, res) {
-  res.sendFile(path.join("./db/db.json"));
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(stickySock, "notes.html"));
 });
-
-app.get("/api/notes/:id", function (req, res) {
-  let savedNotes = JSON.parse(fs.readFileSync("/db/db.json", "utf8"));
-  res.json(savedNotes[Number(req.params.id)]);
+//anything not designated routed to index.html
+app.get("*", function(req, res) {
+    res.sendFile(path.join(stickySock, "index.html"));
 });
-
-app.get("*", function (req, res) {
-  res.sendFile(path.join(stickySock, "index.html"));
+//route for API of notes using database data
+app.get("/api/notes", function(req, res) {
+    notes = JSON.parse(fs.readFileSync("db/db.json"));
+    res.json(notes);
 });
-
-app.post("/api/notes", function (req, res) {
-  let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
-  let newNote = req.body;
-  let uniqueID = (savedNotes.length).toString();
-  newNote.id = uniqueID;
-  savedNotes.push(newNote);
-  fs.writeFileSync("./db/db.json", JSON.stringify(savedNotes));
-  res.json(savedNotes);
+//post to API notes
+app.post("/api/notes", function(req, res) {
+    let newNote = {
+        title: req.body.title,
+        text: req.body.text,
+        id: uuid.v4()
+    }
+    console.log(newNote);
+    notes.push(newNote);
+    fs.writeFileSync("db/db.json", JSON.stringify(notes));
+    res.json(newNote);
 })
 
-app.delete("/api/notes/:id", function (req, res) {
-  let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
-  let noteID = req.params.id;
-  let newID = 0;
-  savedNotes = savedNotes.filter(currNote => {
-    return currNote.id != noteID;
-  })
-  for (currNote of savedNotes) {
-    currNote.id = newID.toString();
-    newID++;
-  }
-  fs.writeFileSync("./db/db.json", JSON.stringify(savedNotes));
-  res.json(savedNotes);
+app.delete("/api/notes/:id", function(req, res) {
+    res.json(notes.filter(note => note.id !== parseInt(req.params.id)));
 })
 
-app.listen(PORT, function () {
-  console.log("Hacking the port at: " + PORT);
+module.exports = notes;
+app.listen(PORT, () => {
+    console.log("Hacking the port" + PORT);
 });
+
