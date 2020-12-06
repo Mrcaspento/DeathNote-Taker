ea4a64ef58e44f1748eca0368d90c59e343806e9
Caspento <Mrcaspento@gmail.com>
2020-12-05 22:38:25 -0500

    syntax

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.get("/", function(req, res) {
 });
 
 app.get("/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "public/notes"));
+    res.sendFile(path.join(__dirname, "public/notes.html"));
 });
 
 app.get("*", function(req, res) {
